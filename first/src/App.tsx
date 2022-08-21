df4a28f729d812f00b6b0c75333be2a7022d5d31
subhashshell <subhash.mahimaluri@shell.com>
2022-08-21 14:58:42 +0530

    test

--- first/src/App.tsx
+++ first/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <h1 data-testid="app-header">Workflow React First App</h1>
-      <p>Test workflow for unit test coverage and cypress new test</p>
+      <p>Test workflow for unit test coverage and cypress new test 1</p>
       <Test />
       <New />
       <Latest />
