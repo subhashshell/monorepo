fd661002e1f513db8b5f0afcdf9cd1ebdbb3075a
Subhash Mahimaluri <subhash.mahimaluri@shell.com>
2022-08-27 20:12:27 +0530

    Storybook code

--- first/src/New.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import App from './New';
-
-test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Unit test work flow2/i);
-    expect(linkElement).toBeInTheDocument();
-  });
